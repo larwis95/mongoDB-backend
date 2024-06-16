021e891807e265bebddcc752f411a567a55638ba
Larry Wisniewski <larryw48390@gmail.com>
2024-06-16 00:09:32 -0400

    finished API

--- controllers/thought-api.js
+++ controllers/thought-api.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/extensions */
 import express from 'express';
-import { Thought } from '../models/index.js';
+import { Thought, User } from '../models/index.js';
 
 const router = express.Router();
 
@@ -8,6 +8,7 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     try {
         const thoughtData = await Thought.find()
+        .populate({ path: 'reactions', select: '-__v' });
         res.json(thoughtData);
     } catch (err) {
         console.log(err);
@@ -18,7 +19,8 @@ router.get('/', async (req, res) => {
 // GET a single thought by id
 router.get('/:id', async (req, res) => {
     try {
-        const thoughtData = await Thought.findOne({ _id: req.params.id });
+        const thoughtData = await Thought.findOne({ _id: req.params.id })
+        .populate({ path: 'reactions', select: '-__v' });
         res.json(thoughtData);
     } catch (err) {
         console.log(err);
@@ -26,18 +28,23 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// POST to create a new thought
+// POST to create a new thought, then add it to user thought sub array field
 
 router.post('/', async (req, res) => {
     try {
         const thoughtData = await Thought.create(req.body);
-        res.json(thoughtData);
+        const userData = await User.findOneAndUpdate(
+            { username: req.body.username },
+            { $push: { thoughts: thoughtData._id } },
+            { new: true }
+        );
+        if (!userData) return res.status(404).json({ message: 'No user found with this username!' });
+        res.json({ thoughtData, userData });
     } catch (err) {
         console.log(err);
         res.status(500).json(err);
     }
 });
-
 // PUT to update a thought by id
 router.put('/:id', async (req, res) => {
     try {
@@ -53,6 +60,36 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     try {
         const thoughtData = await Thought.findOneAndDelete({ _id: req.params.id });
+        res.json(thoughtData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// POST to create a reaction stored in a single thought's reactions array field
+router.post('/:thoughtId/reactions', async (req, res) => {
+    try {
+        const thoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $push: { reactions: req.body } },
+            { new: true, runValidators: true }
+        );
+        res.json(thoughtData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// DELETE to pull and remove a reaction by the reaction's reactionId value
+router.delete('/:thoughtId/reactions/:reactionId', async (req, res) => {
+    try {
+        const thoughtData = await Thought.findOneAndUpdate(
+            { _id: req.params.thoughtId },
+            { $pull: { reactions: { reactionId: req.params.reactionId } } },
+            { new: true }
+        );
         res.json(thoughtData);
     } catch (err) {
         console.log(err);
